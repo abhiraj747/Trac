9fcb6024e0978800a86092fc1e7d7b8c006bcad7
abhiraj747 <33094650+abhiraj747@users.noreply.github.com>
2017-10-27 15:12:40 +0530

    solr response json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+
 var bodyParser = require('body-parser');
 
 // Create application/x-www-form-urlencoded parser
@@ -14,17 +15,40 @@ app.get('/', function (req, res) {
 })
 
 app.post('/process_post', urlencodedParser, function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      issueSearchQuery:req.body.issueSearchQuery
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
+	var resultQuery = client.search(client.query().q(req.body.issueSearchQuery),function (err, result) {// Search documents using strQuery 
+	console.log('Inside Search ++++:', result.response);
+    res.send(JSON.stringify(result.response));
+   });
+   
+});
 
 var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
 
-})
+});
+
+var SolrNode = require('solr-node');
+ 
+// Create client 
+var client = new SolrNode({
+    host: '127.0.0.1',
+    port: '8983',
+    core: 'films_shard1_replica_n2',
+    protocol: 'http'
+});
+ 
+// Set Debug Level 
+var client = new SolrNode({
+    host: '127.0.0.1',
+    port: '8983',
+    core: 'films_shard1_replica_n2',
+    protocol: 'http',
+    debugLevel: 'ERROR' // log4js debug level paramter 
+});
+
+ 
+
+
+ 
